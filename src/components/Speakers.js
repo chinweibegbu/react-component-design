71cdb06989a6fec0f237b3d878ea8774c8d4281f
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-09-11 15:11:19 +0100

    Included state management

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import { data } from "../../SpeakerData";
 import Header from "./Header";
 import SpeakerToolbar from "./SpeakersToolbar";
 import SpeakersList from "./SpeakersList";
 
 function Speakers() {
-    return ( 
-        <div className="container-fluid">
-            <Header />
-            <SpeakerToolbar />
+    const [showSessions, setShowSessions] = useState(true);
+    const [theme, setTheme] = useState('light');
+
+    return (
+        <div className={theme === "light" ? "container-fluid light" : "container-fluid dark"}>
+            <Header theme={theme} />
+            <SpeakerToolbar theme={theme} setTheme={setTheme} showSessions={showSessions} setShowSessions={setShowSessions} />
             <SpeakersList data={data} />
         </div>
-     );
+    );
 }
 
 export default Speakers;
